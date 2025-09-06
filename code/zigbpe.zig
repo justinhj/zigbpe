8da422b4168cf92d2ad8a420a4243f9bdb2577e0
Justin Heyes-Jones <justinhj@gmail.com>
2025-09-06 08:28:33 -0700

    counting

--- code/zigbpe.zig
+++ code/zigbpe.zig
@@ -2,6 +2,19 @@ const std = @import("std");
 const SkippingList = @import("skipping_list").SkippingList;
 
 pub fn main() !void {
+    const TokenType = u32;
+    const Pair = struct {
+        first: TokenType,
+        second: TokenType,
+
+        pub fn init(f: TokenType, s: TokenType) Pair {
+            return Pair{
+                .first = f,
+                .second = s,
+            };
+        }
+    };
+
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     const allocator = gpa.allocator();
     defer _ = gpa.deinit();
@@ -48,10 +61,35 @@ pub fn main() !void {
 
     const target_token_size = 512;
     var current_token: usize = 256;
-    var freqs = std.AutoHashMap(u32, []const u8).init(allocator);
+    var most_frequent_pair_frequency: usize = 0;
+    var most_frequent_pair: Pair = Pair.init(0, 0);
+    var freqs = std.AutoHashMap(Pair, usize).init(allocator);
     defer freqs.deinit();
 
     while (current_token < target_token_size) {
+        var it = list.iterator();
+        const this_token = it.next();
+        if (this_token == null) break;
+        const next_token = it.peek();
+        if (next_token == null) break;
+
+        // Lookup the pair in the frequency map
+        const pair = Pair.init(this_token.?, next_token.?);
+        const freq_entry = freqs.get(pair);
+        var this_freq = 0;
+        if (freq_entry) |entry| {
+            this_freq = entry + 1;
+            entry.* += 1;
+        } else {
+            this_freq = 1;
+            try freqs.put(pair, 1);
+        }
+        most_frequent_pair_frequency = 1;
+        most_frequent_pair = Pair.init(this_token.?, next_token.?);
+
+
+    
+
         current_token += 1;
     }
 }
